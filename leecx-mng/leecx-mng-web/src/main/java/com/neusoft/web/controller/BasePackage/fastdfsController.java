e1c23e18c8031ec3323a7cd9a680d17b92470023
yinfengjiujian <duanmeilin911@163.com>
2018-06-19 16:50:54 +0800

    修改fastdfs例子

--- leecx-mng/leecx-mng-web/src/main/java/com/neusoft/web/controller/BasePackage/fastdfsController.java
+++ leecx-mng/leecx-mng-web/src/main/java/com/neusoft/web/controller/BasePackage/fastdfsController.java
@@ -1,5 +1,6 @@
 package com.neusoft.web.controller.BasePackage;
 
+import com.neusoft.common.utils.LeeJSONResult;
 import com.neusoft.components.fastdfs.FastDFSTemplate;
 import com.neusoft.components.fastdfs.FastDfsInfo;
 import com.neusoft.components.fastdfs.exception.FastDFSException;
@@ -8,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,23 +26,23 @@ public class fastdfsController extends BaseController {
 
     @RequestMapping(value = "/UploadAdnDel", method = RequestMethod.GET)
     public void testUploadAndDel() throws FastDFSException {
-        FastDfsInfo rv = fastDFSTemplate.upload("".getBytes(), "txt");
-        System.out.println(rv);
-        fastDFSTemplate.deleteFile(rv);
+        FastDfsInfo fastDfsInfo = fastDFSTemplate.upload("".getBytes(), "txt");
+        System.out.println(fastDfsInfo);
+        fastDFSTemplate.deleteFile(fastDfsInfo);
     }
 
-    @RequestMapping(value = "/testPool",method = RequestMethod.GET)
+    @RequestMapping(value = "/testPool", method = RequestMethod.GET)
     public void testPool() throws InterruptedException {
         Runnable runnable = () -> {
             try {
-                File file = new File("C:\\meinv.jpg");
+                File file = new File("C:\\336d6baa85.jpg");
                 FileInputStream fis = new FileInputStream(file);
                 byte[] b = new byte[fis.available()];
                 fis.read(b);
                 Map<String, String> map = new HashMap<>();
-                FastDfsInfo rv = fastDFSTemplate.upload(b, "jpg", map);
-                System.out.println(rv.getFileAbsolutePath());
-                fastDFSTemplate.deleteFile(rv);
+                FastDfsInfo fastDfsInfo = fastDFSTemplate.upload(b, "jpg", map);
+                System.out.println(fastDfsInfo.getFileAbsolutePath());
+//                fastDFSTemplate.deleteFile(fastDfsInfo);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -49,6 +52,28 @@ public class fastdfsController extends BaseController {
             new Thread(runnable).start();
         }
         Thread.currentThread().join();
+    }
+
+    @RequestMapping(value = "/testUpload", method = RequestMethod.GET)
+    @ResponseBody
+    public LeeJSONResult testUpload() {
+
+        FastDfsInfo fastDfsInfo = null;
+        try {
+            File file = new File("C:\\336d6baa85.jpg");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] b = new byte[fis.available()];
+            fis.read(b);
+            Map<String, String> map = new HashMap<>();
+            fastDfsInfo = fastDFSTemplate.upload(b, "jpg", map);
+            System.out.println(fastDfsInfo.getFileAbsolutePath());
+//            fastDFSTemplate.deleteFile(fastDfsInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FastDFSException e) {
+            e.printStackTrace();
+        }
+        return LeeJSONResult.ok(fastDfsInfo);
     }
 
 }
